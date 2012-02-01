5cfe89a25069f03d0e4209f87f506f0ef0f35779
Dmitry Vykochko <dmitriy@student.su>
2012-02-01 22:20:12 +0300

    Initial source.
    Empty yii application with some configuration.

---
+++ protected/controllers/CatalogController.php
@@ -0,0 +1,25 @@
+<?php
+
+class CatalogController extends Controller
+{
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+
+	public function actionError()
+	{
+	    if($error=Yii::app()->errorHandler->error)
+	    {
+	    	if(Yii::app()->request->isAjaxRequest)
+	    		echo $error['message'];
+	    	else
+	        	$this->render('error', $error);
+	    }
+	}
+}
