43deeb23dbdabe5dfc78020a035c8d7948d919d2
Dmitry Vykochko <dmitriy@student.su>
2012-02-01 22:20:12 +0300

    Initial source.
    Empty yii application with some configuration.

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+<div class="container" id="page">
+	<h1 id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+
+	<div id="mainmenu">
+		<?php echo CHtml::link('Main',array('/'));?>
+		<?php echo CHtml::link('Catalog',array('catalog/'));?>
+	</div><!-- mainmenu -->
+
+	<?php echo $content; ?>
+
+</div><!-- page -->
+
+</body>
+</html>
